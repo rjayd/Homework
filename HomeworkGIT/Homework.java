d136f8ece312c7af630b8c53f733204ccb2d1bb0
rjayd <rjaybdorado@gmail.com>
2021-08-13 23:38:54 -0400

    Third and Last Commit HW

--- HomeworkGIT/Homework.java
+++ HomeworkGIT/Homework.java
@@ -4,8 +4,9 @@ public class Homework {
 
 	public static void main(String[] args) {
 
-		for(int i=1; i<=100; i++) {
-			System.out.println(i);
+		for (int i = 1; i <= 100; i++) {
+			if (i % 2 != 0)
+				System.out.println(i);
 		}
 
 	}
